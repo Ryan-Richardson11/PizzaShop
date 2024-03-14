e4c9fb7087ff7bfa91f0975c4a2ac21f9be8aa90
Ryan Richardson <ryanrich811@gmail.com>
2024-03-14 19:43:00 -0400

    Upated models.py to separate the different sizes in the Menu Model

--- home/models.py
+++ home/models.py
@@ -6,13 +6,17 @@ from django.db import models
 class Topping(models.Model):
     name = models.CharField(max_length=50)
 
+
+class Size(models.Model):
+    size = models.CharField(max_length=25)
+
 # Stores different menu items in the database
 
 
 class Menu(models.Model):
     pizza_type = models.CharField(max_length=50)
     toppings = models.ManyToManyField(Topping, blank=True)
-    size = models.CharField(max_length=50, default='Large')
+    size = models.CharField(Size, max_length=25, default='Large')
     description = models.TextField()
 
 # class Cart(models.Model):
