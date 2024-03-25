5ee44a86435927d853215c3cbd67678a68a175de
Ryan Richardson <ryanrich811@gmail.com>
2024-03-25 12:36:30 -0400

    added bootstrap for css styling and menu button on home page

--- home/urls.py
+++ home/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('home', views.home),
     path('authorized', views.authorized),
-    path('menu', views.menu_list),
+    path('menu', views.menu_list, name="menu.list"),
     path('menu/<int:pk>', views.menu_item_info),
 ]
